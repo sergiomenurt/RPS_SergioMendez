0a6deec453fa581c3823fc6b5e2a6d464a42e060
sergiomenurt <smendezurtaza@danielcastelao.org>
2025-01-07 22:59:45 +0100

    feat(RPS): Añadir funciones de rpsls
    Añadir en el GameAction dos funciones más, en el Victories el resto de opciones de victoria

--- src/RPS.py
+++ src/RPS.py
@@ -2,39 +2,38 @@ import random
 from enum import IntEnum
 from typing import Counter
 
-
 class GameAction(IntEnum):
     Rock = 0
     Paper = 1
     Scissors = 2
-
+    Lizard = 3
+    Spock = 4
 
 class GameResult(IntEnum):
     Victory = 0
     Defeat = 1
     Tie = 2
 
-
 Victories = {
-    GameAction.Rock: GameAction.Scissors,
-    GameAction.Paper: GameAction.Rock,
-    GameAction.Scissors: GameAction.Paper
+    GameAction.Rock: [GameAction.Scissors, GameAction.Lizard],
+    GameAction.Paper: [GameAction.Rock, GameAction.Spock],
+    GameAction.Scissors: [GameAction.Paper, GameAction.Lizard],
+    GameAction.Lizard: [GameAction.Spock, GameAction.Paper],
+    GameAction.Spock: [GameAction.Scissors, GameAction.Rock]
 }
 
 user_history = []
-
 
 def assess_game(user_action, computer_action):
     if user_action == computer_action:
         print(f"User and computer picked {user_action.name}. Draw game!")
         return GameResult.Tie
-    elif Victories[user_action] == computer_action:
+    elif computer_action in Victories[user_action]:
         print(f"{user_action.name} beats {computer_action.name}. You won!")
         return GameResult.Victory
     else:
         print(f"{computer_action.name} beats {user_action.name}. You lost!")
         return GameResult.Defeat
-
 
 def get_computer_action():
     if len(user_history) < 6:
@@ -45,12 +44,10 @@ def get_computer_action():
         most_common_actions = [action for action, count in action_counts.items() 
                                if count == max(action_counts.values())]
         predicted_action = random.choice(most_common_actions)
-        computer_selection = Victories[predicted_action]
+        computer_selection = random.choice(Victories[predicted_action])
 
     print(f"Computer picked {computer_selection.name}.")
     return computer_selection
-
-
 
 def get_user_action():
     game_choices = [f"{game_action.name}[{game_action.value}]" for game_action in GameAction]
@@ -58,11 +55,9 @@ def get_user_action():
     user_selection = int(input(f"\nPick a choice ({game_choices_str}): "))
     return GameAction(user_selection)
 
-
 def play_another_round():
     another_round = input("\nAnother round? (y/n): ")
     return another_round.lower() == 'y'
-
 
 def main():
     while True:
@@ -80,7 +75,6 @@ def main():
 
         if not play_another_round():
             break
-
 
 if __name__ == "__main__":
     main()
